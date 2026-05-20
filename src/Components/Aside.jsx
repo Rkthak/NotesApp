9e185b45bbbe5e6b8dc4d75869e6540298626b11
Rkthak <rkon2609@gmail.com>
2026-05-20 18:08:52 +0530

    make responsive nav

--- src/Components/Aside.jsx
+++ src/Components/Aside.jsx
@@ -3,20 +3,27 @@ import Logo from "./Logo";
 
 const Aside = () => {
   return (
-    <div className="h-screen bg-slate-900 w-70 border-r-slate-700 border-r-2 p-4 flex flex-col justify-between">
-      <div>
-        {/* LOGO */}
-        <Logo />
-
-        {/* ASIDE BUTTONS */}
+    <>
+      <div className="h-screen bg-slate-900 w-70 border-r-slate-700 border-r-2 p-4 hidden lg:flex flex-col justify-between">
         <div>
-          <AsideButton btnName={"Notes"} link={"/"} />
-          <AsideButton btnName={"Archive"} link={"/archive"} />
-          <AsideButton btnName={"Trash"} link={"/trash"} />
+          {/* LOGO */}
+          <Logo />
+
+          {/* ASIDE BUTTONS */}
+          <div>
+            <AsideButton btnName={"Notes"} link={"/"} />
+            <AsideButton btnName={"Archive"} link={"/archive"} />
+            <AsideButton btnName={"Trash"} link={"/trash"} />
+          </div>
         </div>
+        {/* can be added user profile viewer and user info after login --in bottom */}
       </div>
-      {/* can be added user profile viewer and user info after login --in bottom */}
-    </div>
+      <div className="lg:hidden fixed bottom-0 w-full p-4 bg-slate-900 border-t-2 border-slate-700 flex justify-around items-center">
+        <AsideButton btnName={"Notes"} link={"/"} />
+        <AsideButton btnName={"Archive"} link={"/archive"} />
+        <AsideButton btnName={"Trash"} link={"/trash"} />
+      </div>
+    </>
   );
 };
 
--- src/Components/AsideButton.jsx
+++ src/Components/AsideButton.jsx
@@ -6,11 +6,20 @@ const AsideButton = ({ btnName, link }) => {
       <NavLink
         to={link}
         className={({ isActive }) =>
-          `w-full text-slate-50 hover:bg-sky-500 hover:text-sky-200 transition-all px-5 py-4 text-left font-semibold font-['sora'] rounded-2xl mb-3 block ${isActive ? "bg-sky-500" : ""} `
+          `hidden w-full text-slate-50 hover:bg-sky-500 hover:text-sky-200 transition-all px-5 py-4 text-left font-semibold font-['sora'] rounded-2xl mb-3 lg:block ${isActive ? "bg-sky-500" : ""} `
         }
       >
         {btnName}
       </NavLink>
+      <NavLink
+        to={link}
+        className={({ isActive }) =>
+          `lg:hidden hover:text-sky-200 transition-all  text-center font-semibold font-['sora'] ${isActive ? "text-sky-500" : "text-slate-50"} `
+        }
+      >
+        📝 <br />
+        {btnName}
+      </NavLink>{" "}
     </>
   );
 };
