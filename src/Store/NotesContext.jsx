b9565c3eaf6aad7d1139bb8a8c5dd02a2dfc1fed
Rkthak <rkon2609@gmail.com>
2026-05-21 16:12:08 +0530

    add values in localStorage -immutable_state_even_on-reload

--- src/Store/NotesContext.jsx
+++ src/Store/NotesContext.jsx
@@ -1,12 +1,18 @@
 //~ create context
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const NotesContext = createContext();
 
 //~ provide context
 export const NotesContextProvider = ({ children }) => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("Notes")) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem("Notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <NotesContext.Provider value={{ notes, setNotes }}>
