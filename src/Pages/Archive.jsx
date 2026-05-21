04be9071d0790818326c076d624b67906dfd5a18
Rkthak <rkon2609@gmail.com>
2026-05-21 17:50:46 +0530

    archive and unarchive notes

--- src/Pages/Archive.jsx
+++ src/Pages/Archive.jsx
@@ -1,5 +1,91 @@
+import { useContext } from "react";
+import { NotesContext } from "../Store/NotesContext";
+
 const Archive = () => {
-  return <h1 className="text-amber-50">Archive</h1>;
+  const { notes, setNotes } = useContext(NotesContext);
+
+  const handleUnArchive = (e, id) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setNotes(
+      notes.map((note) =>
+        note.id === id ? { ...note, isArchived: false } : note,
+      ),
+    );
+  };
+
+  return (
+    <div className="p-5 h-screen grid grid-rows-[auto_auto_1fr] lg:gap-0.5 gap-4 lg:pb-1 pb-18">
+      <h2 className="text-slate-50 font-['sora'] text-3xl font-semibold">
+        Archive
+      </h2>
+      <div className="scrollbar-none overflow-y-auto min-h-0 grid md:grid-cols-2  gap-4">
+        {notes.filter((note) => note.isArchived).length == 0 ? (
+          <div className="w-full">
+            <h3 className="font-['sora'] text-slate-400 text-xl capitalize">
+              {" "}
+              no note achived. . .
+            </h3>
+          </div>
+        ) : (
+          notes
+            .filter((note) => note.isArchived)
+            .map((note) => (
+              <div className="bg-slate-800 border-2 border-slate-700 rounded-2xl p-3 h-fit  max-w-80 relative">
+                <div className="">
+                  <div className="flex justify-between items-center mb-1">
+                    <h2 className="font-['sora'] text-slate-50 font-semibold text-2xl capitalize">
+                      {note.isPinned ? <span>📌</span> : ""}
+                      {note.title.length > 15
+                        ? note.title.slice(0, 15) + " ..."
+                        : note.title}
+                    </h2>
+                    <span className=""> 📌</span>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <button className="font-['sora'] text-sm text-sky-500 px-3 py-1 whitespace-nowrap bg-slate-800 rounded-full border-2 border-sky-500">
+                      #
+                      {note.tag.length > 8
+                        ? note.tag.slice(0, 6) + "..."
+                        : note.tag}
+                    </button>
+
+                    <span className="text-slate-400 font-['inter'] ml-4">
+                      {note.createdAt}
+                    </span>
+                  </div>
+
+                  <p className=" text-slate-400 font-['inter'] mt-1.5 mb-4">
+                    {note?.description.toString().length > 30
+                      ? note.description.toString().slice(0, 30) + " . . ."
+                      : note.description}
+                  </p>
+
+                  <div className="flex justify-between items-center">
+                    <button className="font-soro text-slate-50 px-4 py-1 border-2 border-slate-700 bg-slate-900 font-semibold cursor-pointer rounded-xl">
+                      edit
+                    </button>
+                    <div className="">
+                      <button
+                        className="font-soro text-slate-50 px-4 py-1 border-2 border-amber-400 bg-amber-400 font-semibold cursor-pointer rounded-xl ml-3 "
+                        onClick={(e) => {
+                          handleUnArchive(e, note.id);
+                        }}
+                      >
+                        restore
+                      </button>
+                      <button className="font-soro text-slate-50 px-4 py-1 border-2 border-red-400 bg-red-400 font-semibold cursor-pointer rounded-xl ml-3">
+                        Trash
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Archive;
